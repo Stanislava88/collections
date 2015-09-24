c6c860346ed209efdd6179e6ff340eaa26a932b3
KrasimirRaikov <raikov.krasimir@gmail.com>
2015-09-24 14:51:50 +0300

    initial commit

---
+++ src/pages/Demo.java
@@ -0,0 +1,66 @@
+package pages;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author raikov.krasimir@gmail.com (Krasimir Raikov)
+ */
+public class Demo {
+
+  public static void main(String[] args) {
+    String[] str = {"s", "p", "m", "t", "r", "l", "z"};
+    List<String> strings = new ArrayList<>(Arrays.asList(str));
+    Pages page = new Pages(strings, 2);
+    List<String> currentPage = page.next();
+
+    Scanner scan = new Scanner(System.in);
+    boolean stop = false;
+    boolean unchanged = true;
+    while (!stop) {
+
+      if (unchanged) {
+        for (Object o : currentPage) {
+          System.out.println(o);
+        }
+      }
+      String scanned = scan.nextLine();
+      switch (scanned) {
+        case "next":
+          currentPage = page.next();
+          unchanged = true;
+          break;
+        case "previous":
+          currentPage = page.previous();
+          unchanged = true;
+          break;
+        case "hasNext":
+          if(page.hasNext()>0) System.out.println("true");
+          else System.out.println("false");
+          unchanged = false;
+          break;
+        case "hasPrevious":
+          System.out.println(page.hasPrevious());
+          unchanged = false;
+          break;
+        case "firstPage":
+          currentPage = page.firstPage();
+          unchanged = true;
+          break;
+        case "lastPage":
+          currentPage = page.lastPage();
+          unchanged = true;
+          break;
+        case "stop":
+          stop = true;
+          break;
+        default:
+          System.out.println("sorry invalid entry ");
+          unchanged = false;
+          break;
+      }
+    }
+  }
+}
---
+++ src/pages/Pages.java
@@ -0,0 +1,80 @@
+package pages;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author raikov.krasimir@gmail.com (Krasimir Raikov)
+ */
+public class Pages {
+  private int pageSize;
+  private int firstElement;
+  private int lastElement;
+  private final List<String> list;
+  private List<String> listError = new ArrayList<>();
+
+  public Pages(List<String> list, int pageSize) {
+    this.pageSize = pageSize;
+    firstElement = 0 - pageSize;
+    lastElement = firstElement + pageSize;
+    this.list = new ArrayList<>(list);
+  }
+
+  public List<String> next() {
+    int c=hasNext();
+
+    if (c>=pageSize) {
+      firstElement += pageSize;
+      lastElement += pageSize;
+      return list.subList(firstElement, lastElement);
+    }
+    if(c>0&&c<pageSize){
+      firstElement+=pageSize;
+      lastElement+=c;
+      return list.subList(firstElement, lastElement);
+    }
+
+    listError.clear();
+    listError.add("Sorry, reached the last page");
+    return listError;
+
+  }
+
+  public List<String> previous() {
+    if(lastElement-firstElement<pageSize){
+      firstElement-=pageSize;
+      lastElement=firstElement+pageSize;
+      return list.subList(firstElement,lastElement);
+    }
+    if (hasPrevious()) {
+      firstElement -= pageSize;
+      lastElement -= pageSize;
+      return list.subList(firstElement, lastElement);
+    } else {
+      listError.clear();
+      listError.add("sorry you are at the first page");
+      return listError;
+    }
+  }
+
+  public int hasNext() {
+    return lastElement<list.size()?list.size()-lastElement:0;
+  }
+
+  public boolean hasPrevious() {
+    return firstElement - pageSize >= 0;
+  }
+
+  public List<String> firstPage() {
+    firstElement = 0;
+    lastElement = firstElement + pageSize;
+    return list.subList(firstElement, lastElement);
+  }
+
+  public List<String> lastPage() {
+    lastElement = list.size();
+    firstElement = lastElement - pageSize;
+    return list.subList(firstElement, lastElement);
+  }
+}
